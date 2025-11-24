9bd743e0336344ddd64e64b559d502fa9f88ce56
DanielHamill <dhamill3@gatech.edu>
2025-11-23 23:20:52 -0500

    initial commit, add discord bot+message publisher

---
+++ fly-on-the-wall/main.py
@@ -0,0 +1,48 @@
+import os
+import sys
+import logging
+
+import discord
+import pika
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DISCORD_TOKEN = os.getenv("TOKEN")
+MESSAGE_BROKER_HOST = os.getenv("MESSAGE_BROKER_HOST")
+EXCHANGE = os.getenv("EXCHANGE")
+
+# establish discord connection
+intents = discord.Intents.default()
+intents.message_content = True
+discord_client = discord.Client(intents=intents)
+
+# establish message broker connection
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host=MESSAGE_BROKER_HOST))
+channel = connection.channel()
+channel.exchange_declare(exchange='discord_messages', exchange_type='fanout')
+
+# get logger
+logger = logging.getLogger("message-producer")
+
+@discord_client.event
+async def on_ready():
+    print(f'We have logged in as {discord_client.user}')
+
+
+def get_message_payload(message: discord.Message) -> str:
+    return ""
+
+
+@discord_client.event
+async def on_message(message):
+    if message.author == discord_client.user:
+        return
+
+    payload = get_message_payload(message)
+    channel.basic_publish(exchange=EXCHANGE, routing_key='', body=payload)
+    connection.close()
+    logger.info("Publishing message.")
+
+discord_client.run()
